1bdc3ba0457e3b00148b2d86c4ced221e88ed1c7
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-02-21 02:06:51 -0500

    testing removed

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
+  /*   test: {
     environment: "jsdom",
     setupFiles: "src/__test__/setup.ts",
-  },
+  }, */
 });
